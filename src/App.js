ac61e847a47d17edab24e820e6575443720c9904
Charles Howard <cdhoward@add123.com>
2019-08-16 16:23:21 -0400

    working on p2p

--- src/App.js
+++ src/App.js
@@ -1,26 +1,68 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Peer from 'peerjs'
+
+import './App.css'
+
+const useCounter = () => {
+  const [count, setCount] = useState(1)
+  const increment = () => setCount(count + 1)
+  const decrement = () => setCount(Math.max(count - 1, 1))
+  return [count, increment, decrement]
 }
 
-export default App;
+const openRoom = () => {
+  console.log('starting')
+
+  const peer = new Peer('8lu3frm45j300000')
+
+  peer.on('open', () => {
+    console.log('id', peer.id, peer)
+  })
+
+  peer.on('error', err => console.log('error', err))
+
+  peer.on('connection', conn => {
+    // setConnections(prevState => [...prevState, conn])
+    console.log('conn', conn)
+
+    // We want to immediately send the newly connected peer the current data.
+    // conn.on('open', () => {
+    //   conn.send(stateRef.current)
+    // })
+  })
+}
+
+openRoom()
+
+// const Connect = ({ id }) => {
+//   const [state, isConnected, error] = useReceivePeerState(id);
+//   return <pre>{JSON.stringify({ state, isConnected, error })}</pre>
+// }
+
+const Player = () => {
+  const [name, setName] = useState('')
+  const [level, addLevel, removeLevel] = useCounter()
+  // const [state, setState, brokerId, connections, error] = usePeerState({ name: 'test' })
+  return (
+    <div>
+      {/* <pre>{JSON.stringify({ state, setState, brokerId, connections, error })}</pre> */}
+      {/* <div><input value={state.name} onChange={ev => setState({ name: ev.target.value })} /></div> */}
+      <div>Level: {level}</div>
+      <div>
+        <button onClick={removeLevel}>down</button>
+        <button onClick={addLevel}>up</button>
+      </div>
+    </div>
+  )
+}
+
+const App = () => {
+  return (
+    <div>
+      <Player />
+    </div>
+  )
+}
+
+export default App
